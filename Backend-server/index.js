364d58522c573776c49b4ed6a348d953bb9e8b53
Mahdie Montazeralzohoor <74259418+mmontazer-2002@users.noreply.github.com>
2024-01-29 21:36:44 +0330

    adding uploading , and deleting and getting part for the index.js file

--- Backend-server/index.js
+++ Backend-server/index.js
@@ -7,11 +7,54 @@ const cors = require('cors')
 app.use(cors());
 app.use(express.json());
 
+//dgNfsO5shtO1GjW
 
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://Bookstore-server:dgNfsO5shtO1GjW@cluster0.fylaw3y.mongodb.net/?retryWrites=true&w=majority";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    //connect client to the server
+    const bookCollections = client.db("BookInventory").collections("books");
+    
+    // insert a book to db : post method   
+    //////////////////////////////////////////
+    app.post("/upload-book" , async(req , res) => {
+      const data = req.body;
+      const result = await bookCollections.insertOne(data);
+      res.send(result);
+    })
+
+    
+    
+    
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    //await client.close();
+  }
+}
+run().catch(console.dir);
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
--- bookshop-frontend/src/components/Navbar.jsx
+++ bookshop-frontend/src/components/Navbar.jsx
@@ -1,8 +1,53 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom';
+import { FaBlog } from "react-icons/fa6";
 
 function Navbar() {
+    const[isMenuOpen , setIsMenuOpen] = useState(false);
+    const[isSticky, setIsSticky] = useState(false);
+
+    //toggle menu
+    const toggleMenue =() => {
+        setIsMenuOpen(!isMenuOpen);
+    }
+
+    useEffect(()=> {
+        const handleScroll =()=>{
+            if(window.scrollY > 100){
+                setIsSticky(true);
+            }
+            else{
+                setIsSticky(false);
+            
+            }
+        }
+        window.addEventListener("scroll",handleScroll);
+
+        return () => {
+            window.addEventListener("scroll",handleScroll);
+        }
+
+    },[])
+
+
+
+    //nav items
+    const navItem =[
+        {link: "Home", path: "/"},
+        {link: "About", path: "/about"},
+        {link: "Shop", path: "/shop"},
+        {link: "Sell Your Books", path: "/admin/dashboard"},
+        {link: "Blog", path: "/blog"}
+    ]
   return (
-    <div>Navbar</div>
+    <header>
+        <nav>
+            <div>
+                {/*logo */}
+                <Link to= "/" className='text-2xl font-bold text-blue-700 items-center gap-2'><FaBlog className='inline-block'/> Books </Link>
+            </div> 
+        </nav>
+    </header>
   )
 }
 
