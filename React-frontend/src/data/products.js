431df7bf2e72fac7bae031ff257e606b169bb895
Mahdie Montazeralzohoor <74259418+mmontazer-2002@users.noreply.github.com>
2024-02-03 02:07:55 +0330

    last updates

---
+++ React-frontend/src/data/products.js
@@ -0,0 +1,39 @@
+import first from '../assets/first.jpg'
+import second from '../assets/second.jpg'
+import third from '../assets/third.jpg'
+import forth from '../assets/forth.jpg'
+import fifth from '../assets/fifth.jpg'
+export const PRODUCTS =[
+    {
+        id : 1,
+        productName : "The hunger games",
+        price : 1000,
+        productImage : first
+    },
+    {
+        id : 2,
+        productName : "Down the river on to the sea",
+        price : 1500,
+        productImage : second
+    },
+    {
+        id : 3,
+        productName : "Fixit",
+        price : 1700,
+        productImage : third
+    }
+    ,
+    {
+        id : 4,
+        productName : "All the Sinners Bleed",
+        price : 1700,
+        productImage : forth
+    } ,
+    {
+        id : 5,
+        productName : "The Heaven & Earth Grocery Store",
+        price : 500,
+        productImage : fifth
+
+    }
+]
